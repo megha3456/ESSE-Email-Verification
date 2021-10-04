be538c621c8a7993d83c4c1627d21dc651a8fbac
megha123 <megha108jha@gmail.com>
2021-10-04 12:31:41 +0530

    Email verification commited

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Verification from './Components/Verification';
+
+function App() {
+  return (
+    <>
+      
+     <Verification/>
+    </>
+  );
+}
+
+export default App;
